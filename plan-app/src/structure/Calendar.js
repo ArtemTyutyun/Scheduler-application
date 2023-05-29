6710d2119784c6fb87778825dc7da2d1ddec5a21
ArtemTyutyun <tutunartem13@gmail.com>
2023-05-29 06:07:05 +0300

    creating a calendar and its features

--- plan-app/src/structure/Calendar.js
+++ plan-app/src/structure/Calendar.js
@@ -3,3 +3,17 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../styles/main.css'; 
 
+function Calendar({ selected, onChange }) {
+    return (
+      <div className='calendar'>
+        <DatePicker 
+          selected={selected} 
+          onChange={onChange} 
+          className='calendar-input'
+        />
+      </div>
+    );
+  }
+  
+  export default Calendar;
+  
